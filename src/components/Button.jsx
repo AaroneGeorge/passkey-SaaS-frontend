234123ee10ffeb1d318a8bf828ab0f450572f066
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-06-16 21:32:05 +0530

    added getStarted links

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "../style";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 
-const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Get Started
-  </button>
-);
+
+
+const Button = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {  
+    navigate("/login"); 
+  };
+
+  return (
+    <button 
+      type="button" 
+      className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none mt-3 ${styles}`}
+      onClick={handleClick}
+      >
+        Get Started
+    </button>
+  )
+}
 
 export default Button;
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,21 +1,32 @@
 import styles from "../style";
 import { arrowUp } from "../assets";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 
-const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
-      <div className={`${styles.flexStart} flex-row`}>
+const GetStarted = () => {
+  const navigate = useNavigate(); // Utilize useNavigate hook for redirection
+
+  const handleClick = () => {
+    navigate("/login"); // Redirect to /login route on click
+  };
+
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      onClick={handleClick} // Attach onClick handler to the component itself
+    >
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain" />
+        </div>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
-          <span className="text-gradient">Get</span>
+          <span className="text-gradient">Started</span>
         </p>
-        <img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain" />
       </div>
-      
-      <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
-        <span className="text-gradient">Started</span>
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 export default GetStarted;
