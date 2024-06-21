24cb18a74458e609448ec2a7cd05188e68a6cfdd
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-06-21 12:06:54 +0530

    "changed navbar"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { close, logo, menu, security } from "../assets";
-import { navLinks } from "../constants";
 
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
 
   return (
@@ -15,17 +13,26 @@ const Navbar = () => {
       </a>
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
+        <li
+          className="font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-10"
+        >
+          <a href="/">Home</a>
+        </li>
+        <li
+          className="font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-10"
+        >
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Documentation</a>
+        </li>
+        <li
+          className="font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-10"
+        >
+          <a href="/#socials">Socials</a>
+        </li>
+        <li
+          className="font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white"
+        >
+          <a href="/login">Login</a>
+        </li>
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
@@ -42,17 +49,26 @@ const Navbar = () => {
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
+            <li
+              className="font-poppins font-medium cursor-pointer text-[16px] text-dimWhite hover:text-white mb-4"
+            >
+              <a href="/">Home</a>
+            </li>
+            <li
+              className="font-poppins font-medium cursor-pointer text-[16px] text-dimWhite hover:text-white mb-4"
+            >
+              <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Documentation</a>
+            </li>
+            <li
+              className="font-poppins font-medium cursor-pointer text-[16px] text-dimWhite hover:text-white mb-4"
+            >
+              <a href="/#clients">Socials</a>
+            </li>
+            <li
+              className="font-poppins font-medium cursor-pointer text-[16px] text-dimWhite hover:text-white"
+            >
+              <a href="/login">Login</a>
+            </li>
           </ul>
         </div>
       </div>
